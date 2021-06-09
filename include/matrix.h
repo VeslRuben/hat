b8932926284568c66887325080303756fc2de430
RNatvik <ronatvik@gmail.com>
2021-06-09 22:57:13 +0200

    fixed matrix class... maybe

--- include/matrix.h
+++ include/matrix.h
@@ -1,34 +1,33 @@
 #ifndef HAT_MATRIX_H
 #define HAT_MATRIX_H
 
-namespace Matrix {
-    class Matrix {
+namespace Matrix
+{
+    class Matrix
+    {
 
     public:
         Matrix();
-
         Matrix(int height, int width);
-
-        Matrix(int height, int width, double *values);
-
+        Matrix(int height, int width, float *values);
+        Matrix(const Matrix &old_obj);
         ~Matrix();
 
-        double get(int row, int col);
-
-        void put(double value, int row, int col);
+        Matrix &operator=(const Matrix &matrix);
 
         void resize(int height, int width);
-
+        float get(int row, int col);
+        void put(float value, int row, int col);
         int getWidth();
-
         int getHeight();
 
-
     private:
+        float *values_;
         int width_, height_;
-        double *values_;
     };
 
     void multiply(Matrix &matA, Matrix &matB, Matrix &result);
+    Matrix multiply(Matrix matA, Matrix matB);
 }
+
 #endif //HAT_MATRIX_H
--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,68 +1,127 @@
 #include "matrix.h"
+#include <iostream>
 
-namespace Matrix {
-    Matrix::Matrix() {
+namespace Matrix
+{
+    Matrix::Matrix()
+    {
         width_ = 1;
         height_ = 1;
-        values_ = nullptr;
+        values_ = new float[1];
     }
 
-    Matrix::Matrix(int height, int width) {
+    Matrix::Matrix(int height, int width)
+    {
         width_ = width;
         height_ = height;
-        values_ = new double[width * height];
+        values_ = new float[width * height];
     }
 
-    Matrix::Matrix(int height, int width, double *values) {
+    Matrix::Matrix(int height, int width, float *values)
+    {
         width_ = width;
         height_ = height;
-        values_ = new double[width_ * height_];
-        for (int i = 0; i < width_ * height_; i++) {
+        values_ = new float[width_ * height_];
+        for (int i = 0; i < width_ * height_; i++)
+        {
             values_[i] = values[i];
         }
     }
 
-    Matrix::~Matrix() {
+    Matrix::Matrix(const Matrix &old_obj)
+    {
+        height_ = old_obj.height_;
+        width_ = old_obj.width_;
+        values_ = new float[height_ * width_];
+        for (int i = 0; i < height_ * width_; i++)
+        {
+            values_[i] = old_obj.values_[i];
+        }
+    }
+
+    Matrix::~Matrix()
+    {
         delete[] values_;
         values_ = nullptr;
     }
 
-    double Matrix::get(int row, int col) {
+    Matrix &Matrix::operator=(const Matrix &matrix)
+    {
+        if (this != &matrix)
+        {
+            height_ = matrix.height_;
+            width_ = matrix.width_;
+            delete[] values_;
+            values_ = new float[height_ * width_];;
+            for (int i = 0; i < height_ * width_; i++)
+            {
+                values_[i] = matrix.values_[i];
+            }
+        }
+        return *this;
+    }
+
+    void Matrix::resize(int height, int width)
+    {
+        height_ = height;
+        width_ = width;
+        delete[] values_;
+        values_ = new float[height_ * width_];;
+    }
+
+    float Matrix::get(int row, int col)
+    {
         return values_[row * width_ + col];
     }
 
-    void Matrix::put(double value, int row, int col) {
+    void Matrix::put(float value, int row, int col)
+    {
         int index = row * width_ + col;
         values_[index] = value;
     }
 
-    void Matrix::resize(int height, int width) {
-        height_ = height;
-        width_ = width;
-        double *ptr = new double[height * width];
-        delete[] values_;
-        values_ = ptr;
-    }
-
-    int Matrix::getWidth() {
+    int Matrix::getWidth()
+    {
         return width_;
     }
 
-    int Matrix::getHeight() {
+    int Matrix::getHeight()
+    {
         return height_;
     }
+    // #####################################################
 
-// #####################################################
-    void multiply(Matrix &matA, Matrix &matB, Matrix &result) {
+    void multiply(Matrix &matA, Matrix &matB, Matrix &result)
+    {
         result.resize(matA.getHeight(), matB.getWidth());
-        for (int i = 0; i < result.getHeight() * result.getWidth(); i++) {
+        for (int i = 0; i < result.getHeight() * result.getWidth(); i++)
+        {
             int row = i / result.getWidth();
             int col = i % result.getWidth();
-            double sum = 0;
-            for (int j = 0; j < matA.getWidth(); j++) {
+            float sum = 0;
+            for (int j = 0; j < matA.getWidth(); j++)
+            {
                 sum += matA.get(row, j) * matB.get(j, col);
             }
             result.put(sum, row, col);
         }
+    }
+
+    
+    Matrix multiply(Matrix matA, Matrix matB)
+    {
+        Matrix result(matA.getHeight(), matB.getWidth());
+        for (int i = 0; i < result.getHeight() * result.getWidth(); i++)
+        {
+            int row = i / result.getWidth();
+            int col = i % result.getWidth();
+            float sum = 0;
+            for (int j = 0; j < matA.getWidth(); j++)
+            {
+                sum += matA.get(row, j) * matB.get(j, col);
+            }
+            result.put(sum, row, col);
+        }
+        return result;
     }
 }
